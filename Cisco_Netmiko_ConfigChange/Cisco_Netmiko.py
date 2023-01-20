5b3eb7fb0e77a41f4d5bae3e1ebaf739c730bb22
RilesChz <rileychiswell@hotmail.com>
2023-01-20 11:33:48 +0000

    Added CSV inventory

--- Cisco_Netmiko_ConfigChange/Cisco_Netmiko.py
+++ Cisco_Netmiko_ConfigChange/Cisco_Netmiko.py
@@ -1,12 +1,20 @@
 from netmiko import ConnectHandler
 import re
+import csv
 
+inventory = csv.DictReader(open('DeviceList'))
 cisco_password = input('Please enter your password')  # Take password as input so don't have to store it in the code
-net_connect = ConnectHandler(device_type="cisco_ios", host="192.168.0.70", username="admin", password=cisco_password)
 
-router_interfaces = re.findall("Ethernet\d.\d", net_connect.send_command("show ip interface brief"))
-for interface in router_interfaces:
-    interface_commands = [f"Interface {interface}", "no shut"]
-    net_connect.send_config_set(interface_commands)
 
-net_connect.save_config()
+for row in inventory:
+    net_connect = ConnectHandler(device_type= row['type'], host= row['hostIP'], username="admin",
+                                 password=cisco_password)
+
+    router_interfaces = re.findall("Ethernet\d.\d", net_connect.send_command("show ip interface brief"))
+    for interface in router_interfaces:
+        interface_commands = [f"Interface {interface}", "no shut"]
+        net_connect.send_config_set(interface_commands)
+
+    net_connect.save_config()
+
+
