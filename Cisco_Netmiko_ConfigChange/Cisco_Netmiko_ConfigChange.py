b3f214353e14a7399af30236b8dcba34b75e28ab
RilesChz <rileychiswell@hotmail.com>
2023-02-04 15:01:42 +0000

    bug fixes

--- Cisco_Netmiko_ConfigChange/Cisco_Netmiko_ConfigChange.py
+++ Cisco_Netmiko_ConfigChange/Cisco_Netmiko_ConfigChange.py
@@ -5,10 +5,10 @@ import csv
 inventory = csv.DictReader(open('DeviceList.csv'))
 cisco_password = input('Please enter your password')  # Take password as input so don't have to store it in the code
 
+print
 
 for row in inventory:
-    net_connect = ConnectHandler(device_type= row['type'], host= row['hostIP'], username="admin",
-                                 password=cisco_password)
+    net_connect = ConnectHandler(device_type= row['OStype'], host= row['hostIP'], username="admin", password=cisco_password)
 
     router_interfaces = re.findall("Ethernet\d.\d", net_connect.send_command("show ip interface brief"))
     for interface in router_interfaces:
--- Cisco_Netmiko_PortCheck/Cisco_Netmiko_PortCheck.py
+++ Cisco_Netmiko_PortCheck/Cisco_Netmiko_PortCheck.py
@@ -1,26 +1,29 @@
-from netmiko import ConnectHandler
+import netmiko
 import re
 import csv
 import pyinputplus
 
 def device_connector(input_type):
     if input_type == 'MAC':     #change the command depending on input type
-        sent_command = [f"show mac address-table interface"]
-    else:
-        sent_command = [f"show ip arp"]
+        sent_command = 'show mac address-table interface'
+    elif input_type == 'IP':
+        sent_command = 'show ip arp'
 
     output = []
 
-    for row in inventory:     #iterate through the device list
-        net_connect = ConnectHandler(device_type=row['type'], host=row['hostIP'], username="admin",
-                                     password=cisco_password)
 
-        router_interfaces = re.findall("Ethernet\d.\d", net_connect.send_command("show ip interface brief")) #scrape the list of interfaces to check
+
+    for row in inventory:     #iterate through the device list
+        try:
+            net_connect = netmiko.ConnectHandler(device_type=row['OStype'], host=row['hostIP'], username='admin', password=cisco_password)
+        except netmiko.exceptions:
+            print(f'Failed to connect to {row["hostname"]}')
+
+        router_interfaces = re.findall('Ethernet\d.\d', net_connect.send_command("show ip interface brief"))  # scrape the list of interfaces to check
 
         for interface in router_interfaces:
             if user_input in net_connect.send_command(f'{sent_command} {interface}'):
                 output.append('Found on device ' + row['hostname'] + ' port ' + interface)
-
     return output
 
 
@@ -33,20 +36,15 @@ cisco_password = input('Please enter your password \n')  #Take password as input
 inventory = csv.DictReader(open('DeviceList.csv'))
 
 while True:
-    while True:
         user_input = input('Please enter an IP or MAC Address to search for \n')  #Validate input is an IP or MAC
         if ip_regex.search(user_input) is not None:
-            print(device_connector('MAC'))
+            print(device_connector('IP'))
             break
         elif mac_regex.search(user_input) is not None:
-            print(device_connector('IP'))
+            print(device_connector('MAC'))
             break
         else:
             print('That is not a valid IP or MAC Address')
-
-    if pyinputplus.inputYesNo('Do you want to search again? [yes/no] \n') == 'no':
-        break
-
 
 
 
